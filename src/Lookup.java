83c1cf8c3f84eaea083170f6f5fca646af36a190
NK <niksklava@gmail.com>
2026-01-12 14:33:55 +0200

    koda formatēšana

--- src/Lookup.java
+++ src/Lookup.java
@@ -1,8 +1,8 @@
 public class Lookup {
-	public class BlackJack{
-		public class Lang{
-			public class Text{
-				//Game text
+	public class BlackJack {
+		public class Lang {
+			public class Text {
+				// Game text
 				public static String PlayerAddsNewCard;
 				public static String DealerAddsNewCard;
 				public static String PlayerCards;
@@ -20,88 +20,96 @@ public class Lookup {
 				public static String ViewResult;
 				public static String Play;
 				public static String Continue;
-				
-				//Winning Text
+
+				// Winning Text
 				public static String Player_Wins;
 				public static String Dealer_Wins;
-				//Buttons
+				// Buttons
 				public static String Title;
 			}
-			public static void test() {//delete this before final release
-				Text.PlayerAddsNewCard="Text.PlayerAddsNewCard";
-				Text.PlayerCards="Text.PlayerCards ";
-				Text.Not_Allowed="not allowed";
-				Text.DealersCards="Bob's Cards";
+
+			public static void test() {// delete this before final release
+				Text.PlayerAddsNewCard = "Text.PlayerAddsNewCard";
+				Text.PlayerCards = "Text.PlayerCards ";
+				Text.Not_Allowed = "not allowed";
+				Text.DealersCards = "Bob's Cards";
 			}
+
 			public static void Eng() {
-				Text.PlayerAddsNewCard="You picked a new card ";
-				Text.DealerAddsNewCard="Dealer picked a new card";
-				Text.PlayerCards="You now have ";
-				Text.Not_Allowed="not allowed";
-				Text.DealersCards="dealer's cards";
-				Text.DealerHas="Dealer has";
-				Text.Points="Points";
-				Text.NoSavedResult="Your result will not be saved";
-				Text.SavedResult="Your result will be saved";
-				Text.YourCards=" Your cards";
-				Text.YouHave="You have";
-				Text.Hit="Hit";
-				Text.Stand="Stand";
-				Text.Exit="Exit";
-				Text.ViewResult="View result";
-				Text.Play="Play";
-				Text.Continue="Continue?\n1 Yes\n2 No";
-				//Winning
-				Text.Player_Wins="You Win";
-				Text.Dealer_Wins="Dealer won";
-				//Buttons
-				Text.Title="BlackJack";
+				Text.PlayerAddsNewCard = "You picked a new card ";
+				Text.DealerAddsNewCard = "Dealer picked a new card";
+				Text.PlayerCards = "You now have ";
+				Text.Not_Allowed = "not allowed";
+				Text.DealersCards = "dealer's cards";
+				Text.DealerHas = "Dealer has";
+				Text.Points = "Points";
+				Text.NoSavedResult = "Your result will not be saved";
+				Text.SavedResult = "Your result will be saved";
+				Text.YourCards = " Your cards";
+				Text.YouHave = "You have";
+				Text.Hit = "Hit";
+				Text.Stand = "Stand";
+				Text.Exit = "Exit";
+				Text.ViewResult = "View result";
+				Text.Play = "Play";
+				Text.Continue = "Continue?\n1 Yes\n2 No";
+				// Winning
+				Text.Player_Wins = "You Win";
+				Text.Dealer_Wins = "Dealer won";
+				// Buttons
+				Text.Title = "BlackJack";
 			}
+
 			public static void Latv() {
-				//Spēles teksts
-				//Game text
-				Text.PlayerAddsNewCard="Pacēlāt jaunu kārti";
-				Text.DealerAddsNewCard="Dīleris pacēla jaunu kārti";
-				Text.PlayerCards="Sūsu kārtis";
-				Text.Not_Allowed="Darbība nav atļauta";
-				Text.DealersCards="Dīlera kārtis";
-				Text.DealerHas="Dīlerim ir ";
-				Text.Points="Punkti";
-				Text.NoSavedResult="Spēle nesaglabās Jūsu rezultātu";
-				Text.SavedResult="Spēle saglabās Jūsu rezultātu";
-				Text.YourCards="Jūsu kārtis ";
-				Text.YouHave="Jums ir";
-				Text.Hit="Vēl kārtis";
-				Text.Stand="Spēlēt";
-				Text.Exit="Iziet";
-				Text.ViewResult="Skatīt rezultātu";
-				Text.Play="Spēlēt";
-				Text.Continue="Turpināt?\n1 Jā\n2 Nē";
-				//Winning
-				Text.Player_Wins="Jūs uzvarējāt";
-				Text.Dealer_Wins="Dīleris uzvarēja";
-				//Pogas
-				Text.Title="Laipni lūdzam BlackJack spēlē";
-				
+				// Spēles teksts
+				// Game text
+				Text.PlayerAddsNewCard = "Pacēlāt jaunu kārti";
+				Text.DealerAddsNewCard = "Dīleris pacēla jaunu kārti";
+				Text.PlayerCards = "Sūsu kārtis";
+				Text.Not_Allowed = "Darbība nav atļauta";
+				Text.DealersCards = "Dīlera kārtis";
+				Text.DealerHas = "Dīlerim ir ";
+				Text.Points = "Punkti";
+				Text.NoSavedResult = "Spēle nesaglabās Jūsu rezultātu";
+				Text.SavedResult = "Spēle saglabās Jūsu rezultātu";
+				Text.YourCards = "Jūsu kārtis ";
+				Text.YouHave = "Jums ir";
+				Text.Hit = "Vēl kārtis";
+				Text.Stand = "Spēlēt";
+				Text.Exit = "Iziet";
+				Text.ViewResult = "Skatīt rezultātu";
+				Text.Play = "Spēlēt";
+				Text.Continue = "Turpināt?\n1 Jā\n2 Nē";
+				// Winning
+				Text.Player_Wins = "Jūs uzvarējāt";
+				Text.Dealer_Wins = "Dīleris uzvarēja";
+				// Pogas
+				Text.Title = "Laipni lūdzam BlackJack spēlē";
+
 			}
 		}
-		public class Views{
-			public static void FirstPlay(){
-			
+
+		public class Views {
+			public static void FirstPlay() {
+
 			}
+
 			public static void AfterGame() {
-			
+
 			}
 		}
+
 		public class GameLogic {
 			public static void Hit() {
-				
+
 			}
+
 			public static void Stand() {
-				
+
 			}
+
 			public static void Exit() {
-				
+
 			}
 		}
 	}
--- src/MainLogic.java
+++ src/MainLogic.java
@@ -3,130 +3,168 @@ import java.util.Scanner;
 
 public class MainLogic {
 	public static Scanner scanner = new Scanner(System.in);
+
 	public class GameRule {
-		public class Game{
-			//Ar spēli saistīti iestatījumi
-			static int MaxGamePoints;//maksimālais punktu skaits default = 21
+		public class Game {
+			// Ar spēli saistīti iestatījumi
+			static int MaxGamePoints;// maksimālais punktu skaits default = 21
 			static int DeclareWin;
-			static boolean Save;//vai saglabāt
+			static boolean Save;// vai saglabāt
 		}
-		public class Dealer{
-			//Dealer iestatījumi
-			static int WhenStop;//punktu skaits, zem kura dealer ņems vēl kārtis
+
+		public class Dealer {
+			// Dealer iestatījumi
+			static int WhenStop;// punktu skaits, zem kura dealer ņems vēl kārtis
 		}
-		public class Player{
-			//Spēlētāja iestatījumi
+
+		public class Player {
+			// Spēlētāja iestatījumi
 			static int Score;
 			static int money;
 			static int Points;
 			static String Name;
-			
+
 		}
 	}
-	public class Player_Action{
+
+	public class Player_Action {
 		static void New_Card() {
-			if(GameRule.Player.Points<=(GameRule.Game.MaxGamePoints)) {
-				System.out.print(Lookup.BlackJack.Lang.Text.PlayerAddsNewCard+" "+cards.card_deck[cards.current]);
-				GameRule.Player.Points=GameRule.Player.Points+cards.card_value[cards.current];
-				cards.current=cards.current+1;
-				System.out.println(Lookup.BlackJack.Lang.Text.PlayerCards+" "+GameRule.Player.Points+" "+Lookup.BlackJack.Lang.Text.Points);
-				}else {System.out.println(Lookup.BlackJack.Lang.Text.Not_Allowed);}
+			if (GameRule.Player.Points <= (GameRule.Game.MaxGamePoints)) {
+				System.out.print(Lookup.BlackJack.Lang.Text.PlayerAddsNewCard + " " + cards.card_deck[cards.current]);
+				GameRule.Player.Points = GameRule.Player.Points + cards.card_value[cards.current];
+				cards.current = cards.current + 1;
+				System.out.println(Lookup.BlackJack.Lang.Text.PlayerCards + " " + GameRule.Player.Points + " "
+						+ Lookup.BlackJack.Lang.Text.Points);
+			} else {
+				System.out.println(Lookup.BlackJack.Lang.Text.Not_Allowed);
+			}
 		}
 	}
+
 	public class cards {
-		static String[] card_deck= {//Starting deck of cards
-				"1♠️","2♠️","3♠️","4♠️","5♠️","6♠️","7♠️","8♠️","9♠️","10♠️","J♠️","Q♠️","K♠️","A♠️",
-				"1♥️","2♥️","3♥️","4♥️","5♥️","6♥️","7♥️","8♥️","9♥️","10♥️","J♥️","Q♥️","K♥️","A♥️",
-				"1♦️","2♦️","3♦️","4♦️","5♦️","6♦️","7♦️","8♦️","9♦️","10♦️","J♦️","Q♦️","K♦️","A♦️",
-				"1♣️","2♣️","3♣️","4♣️","5♣️","6♣️","7♣️","8♣️","9♣️","10♣️","J♣️","Q♣️","K♣️","A♣️",
-				
+		static String[] card_deck = { // Starting deck of cards
+				"1♠️", "2♠️", "3♠️", "4♠️", "5♠️", "6♠️", "7♠️", "8♠️", "9♠️", "10♠️", "J♠️", "Q♠️", "K♠️", "A♠️",
+				"1♥️", "2♥️", "3♥️", "4♥️", "5♥️", "6♥️", "7♥️", "8♥️", "9♥️", "10♥️", "J♥️", "Q♥️", "K♥️", "A♥️",
+				"1♦️", "2♦️", "3♦️", "4♦️", "5♦️", "6♦️", "7♦️", "8♦️", "9♦️", "10♦️", "J♦️", "Q♦️", "K♦️", "A♦️",
+				"1♣️", "2♣️", "3♣️", "4♣️", "5♣️", "6♣️", "7♣️", "8♣️", "9♣️", "10♣️", "J♣️", "Q♣️", "K♣️", "A♣️",
+
 		};
-		static int[] card_value= {//Value of the cards
-				1,2,3,4,5,6,7,8,9,10,10,10,10,1,
-				1,2,3,4,5,6,7,8,9,10,10,10,10,1,
-				1,2,3,4,5,6,7,8,9,10,10,10,10,1,
-				1,2,3,4,5,6,7,8,9,10,10,10,10,1
+		static int[] card_value = { // Value of the cards
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1,
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1,
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1,
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1
 		};
-		static int current=0;
-		static int size =56;
+		static int current = 0;
+		static int size = 56;
 	}
+
 	public static void shuffle() {
-		//Card deck shuffle script // bogo sorts card deck
-		int a,b,temp_int; 
+		// Card deck shuffle script // bogo sorts card deck
+		int a, b, temp_int;
 		String temp;
-		for(int i=0; i<512; i++) {
-			a=(int)(Math.random()*cards.size);
-			b=(int)(Math.random()*cards.size);
-			temp=cards.card_deck[a];temp_int=cards.card_value[a];
-			cards.card_deck[a]=cards.card_deck[b];cards.card_value[a]=cards.card_value[b];
-			cards.card_deck[b]=temp;cards.card_value[b]=temp_int;
+		for (int i = 0; i < 512; i++) {
+			a = (int) (Math.random() * cards.size);
+			b = (int) (Math.random() * cards.size);
+			temp = cards.card_deck[a];
+			temp_int = cards.card_value[a];
+			cards.card_deck[a] = cards.card_deck[b];
+			cards.card_value[a] = cards.card_value[b];
+			cards.card_deck[b] = temp;
+			cards.card_value[b] = temp_int;
 		}
 	}
-	public class Card_value{
+
+	public class Card_value {
 		static String name() {
 			return cards.card_deck[cards.current];
 		}
+
 		static int value() {
 			return cards.card_value[cards.current];
 		}
+
 		static void next() {
-			if(cards.current==cards.size) {
+			if (cards.current == cards.size) {
 				shuffle();
-				cards.current=0;
+				cards.current = 0;
+			} else {
+				cards.current++;
 			}
-			else{cards.current++;}
 		}
 	}
-	public static void setup(){
-		GameRule.Game.MaxGamePoints=21;
-		GameRule.Dealer.WhenStop=14;
-		GameRule.Player.money=10;
-		GameRule.Player.Score=0;
-		GameRule.Game.Save=false;
+
+	public static void setup() {
+		GameRule.Game.MaxGamePoints = 21;
+		GameRule.Dealer.WhenStop = 14;
+		GameRule.Player.money = 10;
+		GameRule.Player.Score = 0;
+		GameRule.Game.Save = false;
 	}
+
 	public static void play() {
-		if(GameRule.Game.Save==true) {System.out.println(Lookup.BlackJack.Lang.Text.NoSavedResult);}else {System.out.println(Lookup.BlackJack.Lang.Text.SavedResult);}
+		if (GameRule.Game.Save == true) {
+			System.out.println(Lookup.BlackJack.Lang.Text.NoSavedResult);
+		} else {
+			System.out.println(Lookup.BlackJack.Lang.Text.SavedResult);
+		}
 		shuffle();
-		int stop=1;
-		int temp_int=0, a, score=0;
-		int dealer_points=0, player_points=0;
+		int stop = 1;
+		int temp_int = 0, a, score = 0;
+		int dealer_points = 0, player_points = 0;
 		do {
-			dealer_points=0; player_points=0;
-			//dealer's hand
+			dealer_points = 0;
+			player_points = 0;
+			// dealer's hand
 			System.out.println(Lookup.BlackJack.Lang.Text.DealersCards);
-			for(int i =0;i<1;i++) {
+			for (int i = 0; i < 1; i++) {
 				System.out.println(cards.card_deck[temp_int]);
-				dealer_points=dealer_points+cards.card_value[temp_int];
-				temp_int=temp_int+1;}
-			System.out.println(Lookup.BlackJack.Lang.Text.DealerHas+" "+dealer_points+" "+Lookup.BlackJack.Lang.Text.Points);
-			//player's hand
+				dealer_points = dealer_points + cards.card_value[temp_int];
+				temp_int = temp_int + 1;
+			}
+			System.out.println(Lookup.BlackJack.Lang.Text.DealerHas + " " + dealer_points + " "
+					+ Lookup.BlackJack.Lang.Text.Points);
+			// player's hand
 			System.out.println(Lookup.BlackJack.Lang.Text.YouHave);
-			for(int i =0;i<2;i++) {
+			for (int i = 0; i < 2; i++) {
 				System.out.println(cards.card_deck[temp_int]);
-				player_points=player_points+cards.card_value[temp_int];
-				temp_int=temp_int+1;}
-			System.out.println(Lookup.BlackJack.Lang.Text.YouHave+" "+player_points+" "+Lookup.BlackJack.Lang.Text.Points);
-			a=1;//game logic
-			while(a==1) {
-				System.out.println("1)"+Lookup.BlackJack.Lang.Text.Hit+"\n2)"+Lookup.BlackJack.Lang.Text.Stand);
+				player_points = player_points + cards.card_value[temp_int];
+				temp_int = temp_int + 1;
+			}
+			System.out.println(
+					Lookup.BlackJack.Lang.Text.YouHave + " " + player_points + " " + Lookup.BlackJack.Lang.Text.Points);
+			a = 1;// game logic
+			while (a == 1) {
+				System.out.println("1)" + Lookup.BlackJack.Lang.Text.Hit + "\n2)" + Lookup.BlackJack.Lang.Text.Stand);
 				a = scanner.nextInt();
-				if(a==1) {
+				if (a == 1) {
 					Player_Action.New_Card();
 				}
 			}
-			if(player_points<=GameRule.Game.MaxGamePoints) {
-			while(dealer_points<GameRule.Dealer.WhenStop) {
-				System.out.println(Lookup.BlackJack.Lang.Text.DealerAddsNewCard+" "+cards.card_deck[temp_int]);
-				dealer_points=dealer_points+cards.card_value[temp_int];
-				temp_int=temp_int+1;
-			}if(dealer_points>GameRule.Game.MaxGamePoints) {System.out.println(Lookup.BlackJack.Lang.Text.Player_Wins+"\n\n");score=score+1;}
-			else {
-				if(dealer_points>=player_points) { System.out.println(Lookup.BlackJack.Lang.Text.Dealer_Wins+"\n\n");score=score-1;}
-				else { System.out.println(Lookup.BlackJack.Lang.Text.Player_Wins+"\n\n");score=score+1;}
+			if (player_points <= GameRule.Game.MaxGamePoints) {
+				while (dealer_points < GameRule.Dealer.WhenStop) {
+					System.out.println(Lookup.BlackJack.Lang.Text.DealerAddsNewCard + " " + cards.card_deck[temp_int]);
+					dealer_points = dealer_points + cards.card_value[temp_int];
+					temp_int = temp_int + 1;
+				}
+				if (dealer_points > GameRule.Game.MaxGamePoints) {
+					System.out.println(Lookup.BlackJack.Lang.Text.Player_Wins + "\n\n");
+					score = score + 1;
+				} else {
+					if (dealer_points >= player_points) {
+						System.out.println(Lookup.BlackJack.Lang.Text.Dealer_Wins + "\n\n");
+						score = score - 1;
+					} else {
+						System.out.println(Lookup.BlackJack.Lang.Text.Player_Wins + "\n\n");
+						score = score + 1;
+					}
+				}
+			} else {
+				System.out.println(Lookup.BlackJack.Lang.Text.Dealer_Wins + "\n\n");
+				score = score - 1;
 			}
-			}else {System.out.println(Lookup.BlackJack.Lang.Text.Dealer_Wins+"\n\n");score=score-1;}
 			System.out.println(Lookup.BlackJack.Lang.Text.Continue);
-			
-			}while(stop==1);
-		}
+
+		} while (stop == 1);
 	}
+}
